144e02348823dfbd937ce912ef82445cb1eb310a
TheCobraBoy2 <landoncs10@gmail.com>
2025-12-12 02:59:54 -0600

    Refactor main function and implement drawLines for rendering

--- src/main.zig
+++ src/main.zig
@@ -1,27 +1,62 @@
 const std = @import("std");
 const Asteroids = @import("Asteroids");
+const rl = @import("raylib");
 
-pub fn main() !void {
-    // Prints to stderr, ignoring potential errors.
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-    try Asteroids.bufferedPrint();
-}
+const Vector2 = rl.Vector2;
+const THICKNESS = 2.0;
 
-test "simple test" {
-    const gpa = std.testing.allocator;
-    var list: std.ArrayList(i32) = .empty;
-    defer list.deinit(gpa); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(gpa, 42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
+const State = struct {
+    shipPos: Vector2,
+};
+var state: State = .{};
 
-test "fuzz example" {
-    const Context = struct {
-        fn testOne(context: @This(), input: []const u8) anyerror!void {
-            _ = context;
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
+fn drawLines(origin: Vector2, scale: f32, points: []const Vector2) void {
+    const Transformer = struct {
+        origin: Vector2,
+        scale: f32,
+
+        fn apply(self: @This(), p: Vector2) Vector2 {
+            return p.scale(self.scale).add(self.origin);
         }
     };
-    try std.testing.fuzz(Context{}, Context.testOne, .{});
+
+    const t = Transformer{ .scale = scale, .origin = origin };
+
+    for (0..points.len) |i| {
+        rl.drawLineEx(
+            t.apply(points[i]),
+            t.apply(points[(i + 1) % points.len]),
+            THICKNESS,
+            .white,
+        );
+    }
+}
+
+pub fn main() !void {
+    const background_color: rl.Color = rl.Color.black;
+    const window_title: *const [9:0]u8 = "Asteroids";
+
+    std.debug.print("Hello, World", .{});
+    rl.initWindow(640 * 2, 480 * 2, window_title);
+    rl.setTargetFPS(60);
+    defer rl.closeWindow();
+
+    while (!rl.windowShouldClose()) {
+        rl.beginDrawing();
+        defer rl.endDrawing();
+
+        rl.clearBackground(background_color);
+
+        drawLines(
+            Vector2.init(40, 40),
+            32.0,
+            &.{
+                Vector2.init(-0.4, -0.5),
+                Vector2.init(0.0, 0.5),
+                Vector2.init(0.4, -0.5),
+                Vector2.init(0.3, -0.4),
+                Vector2.init(-0.3, -0.4),
+            },
+        );
+    }
 }
